c5d4135bd411422075b86c1f69fe4dd732fe412c
Rafaela Cunha <rafaelaqueen2022@gmail.com>
2024-08-17 00:35:17 +0100

    atualizado

--- Config/Database.js
+++ Config/Database.js
@@ -1,8 +1,8 @@
 module. exports ={
     dialect: 'postgres',
     host: 'localhost',
-    username: 'usuario',
-    password: 'senha',
+    username: 'postgres',
+    password: 'Renata',
     database: 'apinode',
     define:{
     timestamps:true,
