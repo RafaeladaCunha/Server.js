dff63f8a0b9a5e3a692107253892084606e76b34
Rafaela Cunha <rafaelaqueen2022@gmail.com>
2024-08-27 01:52:28 +0100

    Código controller

--- server.js
+++ server.js
@@ -1,47 +1,9 @@
 const express = require('express');
-const axios = require('axios');
+const rotas = require('./routes');
 
 
 const app = express ();
-const port = 3000;
-
-app.get('/' ,(req, res) => {
- res.send ('Hello World');
-
-});
-
-
-app.get('/novarota',(req, res) => {
-    res.send ('Nova rota criada')
-
-    
-});
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-app.get('/consultar-cep/:cep', async (req, res) =>{
-    const cep = req.params.cep;
-    const cepRegex = /^[0-9](5)-? [0-9]{3}$/;
-    try{
-        if(!cepRegex.test(cep) ){
-            res.status(400).send(`CEP Inválido. Formato: xxxx-xxx`)
-
-        }
-        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
-        res.json(response.data);
-
-    }catch(error){
-        console.error('Erro ao fazer requisição: ', error);
-        res.status(500).send ( `Erro ao consultar o CEP`);
-    }
-
-})
-
-app.listen(port,() => {
-    console.log(`Servidor Rodando em https://localhost:${port}`);
-     
-})
+app.use(express.json());
+app.use('/api', rotas);
+   const PORT = process.env.PORT || 3000;
+   app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`)); 
